56e000af5d24a29615319e5aa24fb3ca7759a35e
moon <liuyc@fsmeeting.com>
2018-04-13 22:49:44 +0800

    ss

--- hadoop-learning-01/src/test/java/com/fsmeeting/AppTest.java
+++ hadoop-learning-01/src/test/java/com/fsmeeting/AppTest.java
@@ -1,6 +1,8 @@
 package com.fsmeeting;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -16,8 +18,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- * 47.98.108.252 node-1
- * 39.105.29.24 node-2
  * Unit test for simple App.
  */
 public class AppTest {
@@ -32,13 +32,39 @@ public class AppTest {
         fileSystem = FileSystem.get(new URI("hdfs://node-2:9000"), conf, "root");
     }
 
+    @Test
+    public void testExists() throws IOException {
+        Path path = new Path("/test01.md");
+        System.out.println(fileSystem.exists(path));
+    }
+
+    @Test
+    public void testAppend() throws IOException {
+      /*  Path path = new Path("/test01.md");
+        fileSystem.append(path);
+        System.out.println();*/
+    }
+
+    @Test
+    public void testCreate() throws IOException {
+        Path path = new Path("/1.txt");
+        FSDataOutputStream out = fileSystem.create(path, true);
+        out.write("hello hadoop".getBytes());
+    }
+
+    @Test
+    public void testCreateNewFile() throws IOException {
+        Path path = new Path("2.txt");
+        fileSystem.createNewFile(path);
+    }
+
     /**
      * hdfs 2 local file system
      */
     @Test
     public void testDownload() throws IOException {
         InputStream in = fileSystem.open(new Path("/test01.md"));
-        OutputStream out = new FileOutputStream("x://test//tmp01.txt");
+        OutputStream out = new FileOutputStream("d://test//tmp01.txt");
         IOUtils.copyBytes(in, out, 1024, true);
     }
 
@@ -46,6 +72,72 @@ public class AppTest {
     public void testUpload() throws IOException {
 
         fileSystem.copyFromLocalFile(new Path("x://test//up.txt"), new Path("/"));
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        Path path = new Path("/up.txt");
+        fileSystem.delete(path);
+    }
+
+    /**
+     * 文件状态
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetFileStatus() throws IOException {
+        Path path = new Path("/1.txt");
+        FileStatus status = fileSystem.getFileStatus(path);
+        System.out.println(status.getModificationTime());
+    }
+
+    /**
+     * 文件状态，可以获得文件夹信息
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testListFile() throws IOException {
+        Path path = new Path("/");
+        FileStatus[] status = fileSystem.listStatus(path);
+        for (FileStatus fileStatus : status) {
+            System.out.println(fileStatus.getPath());
+        }
+    }
+
+    /**
+     * 创建文件夹
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testMkdirs() throws IOException {
+        Path path = new Path("/test");
+        fileSystem.mkdirs(path);
+    }
+
+    /**
+     * 移动本地文件到hdfs
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testMoveFromLocalFile() throws IOException {
+        Path path = new Path("/test");
+        fileSystem.moveFromLocalFile(new Path("C:\\Users\\liuyc\\Desktop\\setenv.sh"), path);
+    }
+
+    /**
+     * 重命名
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testRename() throws IOException {
+        Path path = new Path("/test01.md");
+        Path dest = new Path("/1.txt");
+        fileSystem.rename(path, dest);
     }
 
     @After
